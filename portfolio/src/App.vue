225686e7fae58b3b93ba2ab30d0f39fe6e85f7e3
Sean Yager <contact@seanyager.com>
2020-11-16 21:25:33 -0600

    removed old pages and made mobile page instead

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,36 +1,51 @@
 <template>
   <div id="app">
-    <LandingPage/>
-    <ArtPage v-if="true"/>
-    <AudioPage v-if="true"/>
-    <GamesPage v-if="true"/>
-    <WebDevPage v-if="true"/>
-    <ResumePage v-if="true"/>
+    <NavBar/>
+    <HomePage/>
+
+    <LandingPage v-if="false"/>
+    <ResumePage v-if="false"/>
   </div>
 </template>
 
 <script>
 import '@/assets/styles/style.css'
 
+
+
+import NavBar from '@/components/NavBar.vue'
+import HomePage from '@/components/HomePage.vue'
 import LandingPage from '@/components/LandingPage.vue'
-import ArtPage from '@/components/ArtPage.vue'
-import AudioPage from '@/components/AudioPage.vue'
-import GamesPage from '@/components/GamesPage.vue'
-import WebDevPage from '@/components/WebDevPage.vue'
 import ResumePage from '@/components/ResumePage.vue'
 
 export default {
   name: 'App',
   components: {
+    NavBar,
+    HomePage,
     LandingPage,
-    ArtPage,
-    AudioPage,
-    GamesPage,
-    WebDevPage,
     ResumePage,
   }
 }
 </script>
 
 <style>
+
+#app {
+    background-image: url('../src/assets/imgs/portfolio-stack.png');
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height:100vh;
+}
+
+/* Mobile Port */
+@media (min-width: 667px) {
+
+}
+
+/* Tablets and Up */
+@media (min-width: 768px) {
+
+}
 </style>
