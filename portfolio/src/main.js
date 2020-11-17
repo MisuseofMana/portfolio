afaeaed454c76eccb9d565e280a654af88118b45
Sean Yager <contact@seanyager.com>
2020-11-16 21:25:42 -0600

    added new icons

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Icon from 'vue-awesome/components/Icon'
+import Toasted from 'vue-toasted';
 
 import 'vue-awesome/icons/keyboard'
 import 'vue-awesome/icons/file-code'
@@ -17,8 +18,14 @@ import 'vue-awesome/icons/film'
 import 'vue-awesome/icons/microphone'
 import 'vue-awesome/icons/code'
 import 'vue-awesome/icons/people-carry'
+import 'vue-awesome/icons/hamburger'
+import 'vue-awesome/icons/arrow-circle-left'
+import 'vue-awesome/icons/arrow-circle-right'
+import 'vue-awesome/icons/link'
 
 Vue.component('v-icon', Icon);
+
+Vue.use(Toasted)
 
 Vue.config.productionTip = false
 
