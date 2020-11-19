4c26370000260208e78a23d4fb8f393fc95e55c8
Sean Yager <contact@seanyager.com>
2020-11-19 15:02:43 -0600

    the heart of the site, used to dynamically display links

--- portfolio/src/components/CardTemplate.vue
+++ portfolio/src/components/CardTemplate.vue
@@ -1,34 +1,51 @@
 <template>
-    <section class="cardTemplate" :key="links[this.currentNumber].title">
+    <section class="cardTemplate flexColumn">
+            <transition name="fade" mode="out-in">
+                <section :key="this.links[this.currentNumber].title">
+                    <section class='title'>
+                        <h1>{{ links[this.currentNumber].title}}</h1>
+                        <p> {{ links[this.currentNumber].category}} </p>
+                    </section>
+                </section>
+            </transition>
 
-        <p class="leftArrow flexCenter" @click="backNavigation"><v-icon name="arrow-circle-left" scale="2"></v-icon></p>
-        
-        <h1 class="title">{{ links[this.currentNumber].title}}</h1>
-            
-        <a class="cardImage" :class="{'notAllowed' : !links[this.currentNumber].link, 'allowed' : links[this.currentNumber].link}" :style="{backgroundImage: `url(${require(`@/assets/imgs/${links[this.currentNumber].background}.png`)})`}" :href="links[this.currentNumber].link" target="_blank" @click="checkLink"></a>
-    
-        <section class="description">
-            <p v-for="(sentences, index) in links[this.currentNumber].description" :key='sentences[0]+index'>{{ sentences }}</p>
-        </section>
+            <section class="flexRow navRow">
+                <v-icon @click="backNavigation" name="arrow-circle-left" scale="3"></v-icon>
+                
+                <transition name="fade" mode="out-in">
+                    <a :key="this.links[this.currentNumber].title + 'img'" class="cardImage ifDesktop" :class="{'notAllowed' : !links[this.currentNumber].link, 'allowed' : links[this.currentNumber].link}" :style="{backgroundImage: `url(${require(`@/assets/imgs/${links[this.currentNumber].background}.png`)})`}" :href="links[this.currentNumber].link" target="_blank" @click="checkLink"></a>
+                </transition>
 
-        <p class="rightArrow flexCenter" @click="forwardNavigation"><v-icon name="arrow-circle-right" scale="2"></v-icon></p>
+                <transition name="fade" mode="out-in">
+                    <a :key="this.links[this.currentNumber].title + 'imgSmall'" class="cardImage ifMobile" :class="{'notAllowed' : !links[this.currentNumber].link, 'allowed' : links[this.currentNumber].link}" :style="{backgroundImage: `url(${require(`@/assets/imgs/${links[this.currentNumber].smallBackground}.png`)})`}" :href="links[this.currentNumber].link" target="_blank" @click="checkLink"></a>
+                </transition>
+
+                <v-icon @click="forwardNavigation" name="arrow-circle-right" scale="3"></v-icon>
+            </section>
+                    
+                <transition name="fade" mode="out-in">
+                    <section :key="this.links[this.currentNumber].title  + 'describe'" class="description">
+                        <p v-for="(sentences, index) in links[this.currentNumber].description" :key='sentences[0]+index'>{{ sentences }}</p>
+                    </section>
+            </transition>
     </section>
 </template>
 
 <script>
+import {links} from '../plugins/links.js'
+
     export default {
         name: 'CardTemplate',
-        props:['links'],
         data() {
             return {
                 navigationAlert: {
                     theme: "outline", 
                     position: "bottom-center",
-                    duration:3500,
                     singleton:true
                 },
                 currentNumber: 0,
                 navigated: 0,
+                links,
             }
         },
         methods: {
@@ -59,55 +76,84 @@
                 this.$toasted.show('Tip: Try clicking an image to find out more about that project.', this.navigationAlert)
             },
         },
+        computed: {
+            beforeNumber: function () {
+                if (this.currentNumber === 0 ) return this.links.length-1
+                else return this.currentNumber - 1
+            },
+            afterNumber: function () {
+                if (this.currentNumber === this.links.length-1) return 0
+                else return this.currentNumber + 1
+            },
+        },
         created: function() {
             this.currentNumber = Math.floor(Math.random() * Math.floor(this.links.length-1));
+        },
+        mounted: function () {
+            console.log(this.links)
         }
     }
 </script>
 
 <style scoped>
 /* Mobile Styles */
-.cardTemplate {
-    display:grid;
-    height:250px;
-    grid-template-rows:1fr 1fr 1fr;
-    grid-template-columns:1fr 3fr 1fr;
-    grid-template-areas:
-    "title title title"
-    "arrowLeft card arrowRight"
-    "about about about"
-    ;
-    border-radius:20px;
-    margin:0 auto;
-    text-decoration:none;
+
+.cardNav {
+    height:150px;
+    width:150px;
+    margin: 0 0 20px 0;
 }
 
 .title {
-    grid-area: title;
-    font-size:8vw;
+    margin-bottom:15px;
 }
 
-.cardImage {
-    display:block;
-    border-radius:20px;
+.title h1 {
+    font-size:8vw;
+    margin:0px;
+}
+
+.title p {
+    font-size:4vw;
+    margin:0px;
+}
+
+.navRow {
     width:100%;
-    min-height:140px;
+}
+
+.cardSizing {
+    width:7%;
+    height:100px;
     background-position: center;
     background-size: cover;
-    grid-area:card;
+    border-radius:20px;
+    position:relative;
+    opacity:.6;
+}
+
+a.cardImage {
+    display:block;
+    border-radius:20px;
+    width:150px;
+    box-shadow:1px 1px 3px black;
+    height:150px;
+    background-position: center;
+    background-size: cover;
+}
+
+a.cardImage.ifDesktop {
+    display:none;
+}
+
+a.cardImage.ifMobile {
+    display:block;
 }
 
 .description {
-    grid-area:about;
+    margin:10px;
     font-size:4vw;
-}
-
-.leftArrow {
- grid-area:arrowLeft;
-}
-
-.rightArrow {   
-    grid-area:arrowRight;
+    height:100px;
 }
 
 .notAllowed {
@@ -119,79 +165,8 @@
 }
 
 /* Mobile Portrtait */
-@media (min-width: 667px) {
-.cardBody, a {
-    width:400px;
-    height:550px;
-    display:grid;
-    grid-template-rows:5fr 1fr 1fr 1fr 1fr;
-    grid-template-columns:550px;
-    border-radius:20px;
-    margin-bottom:20px;
-    text-decoration:none;
-}
+@media (min-width: 500px) {
 
-.allowed a:hover > .cardImage {
-    filter:blur(2px) brightness(1.2);
-}
-
-.notAllowed a:hover > .cardImage {
-    filter:blur(2px) brightness(.3) grayscale(1);
-}
-
-.cardBody a:hover > .cardOverlay h1, a:hover > .cardOverlay p, a:hover {
-    color:rgb(255, 255, 255);
-}
-
-a:hover > .cardOverlay {
-    opacity:1;
-    background:rgba(0,0,0,.6);
-    border-radius:20px;
-}
-
-.cardBody:not(:last-child) {
-    margin-right:20px;
-}
-
-.cardImage {
-    border-radius:20px;
-    width:400px;
-    height:550px;
-    background-position: top;
-    background-size: cover;
-    grid-row:1 / 6;
-    grid-column: 1 / 3;
-    z-index:0;
-}
-
-.cardOverlay {
-    width:400px;
-    height:550px;
-    grid-row:1 / 6;
-    grid-column:1 / 3;
-    display:flex;
-    flex-direction:column;
-    justify-content: center;
-    align-items:center;
-    text-align:center;
-    background: rgba(0,0,0,0);
-    color:white;
-    z-index:1;
-    opacity:0;
-}
-
-h1 {
-    color:white;
-    font-size:2em;
-}
-
-.notAllowed {
-    cursor:not-allowed;
-}
-
-.allowed{
-    cursor:pointer;
-}
 }
 
 /* Tablets and Up */
