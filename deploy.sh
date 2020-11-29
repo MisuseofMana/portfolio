0d90680475d2c39c27eb3b695ebea35abeca420e
Sean Yager <contact@seanyager.com>
2020-11-29 15:52:38 -0600

    new docs folder

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ cd docs
 echo 'seanyager.com' > CNAME
 
 git init
-git add -A
-git commit -m 'deploy'
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
