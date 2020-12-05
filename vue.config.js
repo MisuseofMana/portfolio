c5a65a948a21b6f661bf36244d7deedf870737ca
Sean Yager <contact@seanyager.com>
2020-12-04 18:56:22 -0600

    new deployment - this should work! WHY ISNT IT WORKING?

--- vue.config.js
+++ vue.config.js
@@ -3,14 +3,14 @@ module.exports = {
     outputDir: "docs",
     publicPath: '/docs/',
     indexPath:'index.html',
-    // filenameHashing: false,
-    chainWebpack: (config) => {
-        config.module
-            .rule('txt')
-            .test(/\.txt$/)
-            .use('raw-loader')
-            .loader('raw-loader')
-            .end();
-    },
+    filenameHashing: false,
+    // chainWebpack: (config) => {
+    //     config.module
+    //         .rule('txt')
+    //         .test(/\.txt$/)
+    //         .use('raw-loader')
+    //         .loader('raw-loader')
+    //         .end();
+    // },
 }
 
