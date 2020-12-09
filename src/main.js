a378d4c7f6db6d79f7dcf07731830a4713f40f3c
Sean Yager <contact@seanyager.com>
2020-12-08 22:31:25 -0600

    decluttering old main.js

--- src/main.js
+++ src/main.js
@@ -1,33 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import Icon from 'vue-awesome/components/Icon'
+
 import Toasted from 'vue-toasted';
 
 import Links from './plugins/links.js'
 
-import 'vue-awesome/icons/keyboard'
-import 'vue-awesome/icons/file-code'
-import 'vue-awesome/icons/laptop-code'
-import 'vue-awesome/icons/filter'
-import 'vue-awesome/icons/cubes'
-import 'vue-awesome/icons/code-branch'
-import 'vue-awesome/icons/fire-alt'
-import 'vue-awesome/icons/database'
-import 'vue-awesome/icons/camera'
-import 'vue-awesome/icons/bezier-curve'
-import 'vue-awesome/icons/edit'
-import 'vue-awesome/icons/film'
-import 'vue-awesome/icons/microphone'
-import 'vue-awesome/icons/code'
-import 'vue-awesome/icons/people-carry'
-import 'vue-awesome/icons/hamburger'
-import 'vue-awesome/icons/arrow-circle-left'
-import 'vue-awesome/icons/arrow-circle-right'
-import 'vue-awesome/icons/link'
-import 'vue-awesome/icons/window-close'
 
-Vue.component('v-icon', Icon);
+
+
 
 Vue.use(Toasted);
 Vue.use(VueRouter);
