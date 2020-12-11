c57f1c5aab721e59e80e59ca65ecc06335c7d3fd
Sean Yager <contact@seanyager.com>
2020-12-10 20:56:07 -0600

    modularized resume with components | rearranged html order

--- pages/resume.vue
+++ pages/resume.vue
@@ -4,106 +4,128 @@
             <header>
               <h1>RESUME OF A LVL {{this.devLevel}} WEB DEV</h1>
               <h2>Sean A. Yager</h2>
-              <h3 class="centerText"> Web Designer, Comics Artist, Illustrator, Puzzle Solver, Storyteller, Friendly Guy</h3>
+              <h3> Web Designer, Comics Artist, Illustrator, Puzzle Solver, Storyteller, Friendly Guy</h3>
             </header>
+
+            <section class="experience">
+              <h1>Experience</h1>
+              <ResumeBlock 
+                type="job"  
+                title="Freelance Web Developer"
+                subtitle="Austin, TX"
+                role="Owner &amp; Operator"
+                startDate="Feb 2013"
+                endDate="Present"
+                :list="[
+                  'cooperate with individual and business branding through art, websites, and iconography.',
+                  'work spans over 7 years, clientel ranges from college campus programs, to Youtube channels, Kickstarters, Twitch streamers, family owned businesses, individuals etc.', 
+                ]"
+              />
+              <ResumeBlock
+                  type='job'  
+                  title="Creative Converting"
+                  subtitle="Indianapolis, IN"
+                  role="Merchandising Coordinator"
+                  startDate="AUG 2018"
+                  endDate="SEPT 2019"
+                  :list="[
+                  'printed accurate product samples, cut, assembled, and packaged for mockups.',
+                  'managed and fulfilled multiple sample orders on deadlines for trade shows, sales meetings, and clients.',
+                  'navigated physical and digital catalogs to accurately assemble hundreds of name brand products, and custom trade show displays',
+                  'suggested ways to improve efficiancy in workplace processes'
+                  ]"
+                />
+            </section>
 
             <section class="skills">
               <h1>Skills</h1>  
                 <section class="flexRowWrap">
-                  <p><Icon name="keyboard"/> Mac &amp; Windows OSX</p>
-                  <p><Icon name="file-code"/> HTML 5</p>
-                  <p><Icon name="code"/> CSS 3</p>
-                  <p><Icon name="laptop-code"/> Javascript ES6</p>
-                  <p><Icon name="cubes"/> Visual Studio Code</p>
-                  <p><Icon name="code-branch"/> GitHub</p>
-                  <p><Icon name="people-carry"/> Node.js</p>
-                  <p><Icon name="cubes"/> Vue.js</p>
-                  <p><Icon name="cubes"/> React.js</p>
-                  <p><Icon name="fire-alt"/> Google Firebase</p>
-                  <p><Icon name="database"/> SQL</p>
-                  <p><Icon name="camera"/> Adobe Photoshop</p>
-                  <p><Icon name="bezier-curve"/> Adobe Illustrator</p>
-                  <p><Icon name="edit"/> Adobe InDesign</p>
-                  <p><Icon name="film"/> Adobe Premier</p>
-                  <p><Icon name="microphone"/> Adobe Audition</p>
+                  <ResumeChip iconName="keyboard" skill="Mac &amp; Windows OSX" />
+                  <ResumeChip iconName="file-code" skill="HTML 5"/>
+                  <ResumeChip iconName="code" skill="CSS 3"/>
+                  <ResumeChip iconName="laptop-code" skill="Javascript ES6"/>
+                  <ResumeChip iconName="cubes" skill="Visual Studio Code"/>
+                  <ResumeChip iconName="code-branch" skill="Github"/>
+                  <ResumeChip iconName="people-carry" skill="NPM"/>
+                  <ResumeChip iconName="cubes" skill="Vue JS"/>
+                  <ResumeChip iconName="cubes" skill="React JS"/>
+                  <ResumeChip iconName="cubes" skill="Nuxt JS"/>
+                  <ResumeChip iconName="fire-alt" skill="Google Firebase"/>
+                  <ResumeChip iconName="database" skill="SQL"/>
+                  <ResumeChip iconName="camera" skill="Adobe Photoshop"/>
+                  <ResumeChip iconName="bezier-curve" skill="Adobe Illustrator"/>
+                  <ResumeChip iconName="edit" skill="Adobe InDesign"/>
+                  <ResumeChip iconName="film" skill="Adobe Premier"/>
+                  <ResumeChip iconName="microphone" skill="Adobe Audition"/>
                 </section>
             </section>
-            
-            <section class="experience">
-              <h1>Experience</h1>
-              <section class="block">
-                <section class="flexRowStart">
-                  <h3>Misuse of Mana LLC, Austin, TX</h3>
-                  <p> | Owner and Operator</p>
-                </section>
-                <p class="date">FEB 2013 - PRESENT</p>
-                <ul>
-                  <li>cooperate with individual and business branding through art, websites, and iconography.</li>
-                  <li>work spans over 7 years, clientel ranges from college campus programs, to Youtube channels, Kickstarters, Twitch streamers, family owned businesses, individuals etc.</li>
-                </ul>
-              </section>
-              
-              <section class="block">
-                <section class="flexRowStart">
-                  <h3>Creative Converting, Indianapolis, IN</h3>
-                  <p>| Merchandising Coordinator</p>
-                </section>
-                <p class="date">AUG 2018 - SEPT 2019</p>
-                <ul>
-                  <li>printed accurate product samples, cut, assembled, and packaged for mockups.</li>
-                  <li>managed and fulfilled multiple sample orders on deadlines for trade shows, sales meetings, and clients.</li>
-                  <li>navigated physical and digital catalogs to accurately assemble hundreds of name brand products, and custom trade show displays</li>
-                </ul>
-              </section>
-            </section>
-            
+                          
             <section class="education">
               <h1>Education</h1>
+              <ResumeBlock
+                  type='education'
+                  title="Full Stack Bootcamp"
+                  subtitle="Austin Coding Academy"
+                  role="Certified Full Stack Web Developer"
+                  startDate="MAR 2020"
+                  endDate="SEPT 2019"
+                  :list="[
+                  'Completed this biweekly intensive bootcamp course over 7 months during COVID quarantine, I receieved a full stack certification in 2020.',
+                  'The bootcamp covered HTML, CSS, Javascript, React.js, Github, Node.js, Unit Testing, SQL, and various APIs.'
+                  ]"
+                />
 
-              <section class="block">
-                <h3>Full Stack Bootcamp</h3>
-                <h4>Austin Coding Academy</h4>
-                <p class="date">MAR 2020 - SEP 2020</p>
-                <p>Completed this biweekly intensive bootcamp course over 7 months during COVID quarantine, I receieved a full stack certification in 2020.</p>
-                <p>The bootcamp covered HTML, CSS, Javascript, React.js, Github, Node.js, Unit Testing, SQL, and various APIs.</p>
-              </section>
-
-              <section class="block">
-                <h3>IUPUI</h3>
-                <h4>Media Arts and Science</h4>
-                <p class="date">2011 - 2015, 2018</p>
-                <p>Studied various subject matters from digital media, illustration, to cartooning. Each of these focuses helped develop my abilities as a designer, and as a creative mind.</p>
-              </section>
-
-              <section class="block">
-                <h3>Ongoing Research</h3>
-                <h2>LYNDA, Skillshare, Codecademy, MDN</h2>
-                <p class="date">2010 - PRESENT</p>
-                <p>Regularly research topics relevant to my freelance work, including Adobe Suite, new standards in ES6, Javascript, web frameworks, and more to stay relevant in my practice.</p>
-              </section>
+                <ResumeBlock
+                  type='education'
+                  title="Media Arts and Science"
+                  subtitle="IUPUI"
+                  role="Various Studies in Computer Science"
+                  startDate="2011"
+                  endDate="2015, 2018"
+                  :list="[
+                  'Studied various subject matters from digital media, illustration, to cartooning. Each of these focuses helped develop my abilities as a designer, and as a creative mind despite choosing not to continue pursuing college education.'
+                  ]"
+                />
+                
+                <ResumeBlock
+                  type="education"
+                  title="Ongoing Research"
+                  subtitle=""
+                  role="Independent Self-Education"
+                  startDate="2010"
+                  endDate="Present"
+                  :list="[
+                  'Regularly research topics relevant to my freelance work, including Adobe Suite, new standards in ES6, Javascript, web frameworks, and more to stay relevant in my practice.'
+                  ]"
+                />
             </section>
             
             <section class="personal">
               <h1>Personal Work</h1>  
-              <section class="block">
-                <h2>Dungeons of Amara</h2>
-                <h3>Kickstarter Book of Art and Poetry</h3>
-                <p>I successfully crowd funded and published a softback art book titled "Dungeons of Amara" which is a playable RPG dice game filled with artwork and poetry.</p>
-                <a class="button" target="_blank" href="https://www.kickstarter.com/projects/dungeonsofamara/dungeons-of-amara-poems-about-monsters-and-townfol"> <Icon name="link" /> Check it out!</a>
-              </section>
+
+              <ResumeBlock
+                type="project"
+                title="Dungeons of Amara"
+                subtitle="Kickstarter Book of Art and Poetry"
+                description='I successfully crowd funded and published a softback art book titled "Dungeons of Amara" which is a playable RPG dice game filled with artwork and poetry.'
+                url="https://www.kickstarter.com/projects/dungeonsofamara/dungeons-of-amara-poems-about-monsters-and-townfol"
+                linkText="Check It Out!"
+                iconName="book-reader"
+              />
               
-              <section class="block">
-                <h2>Moebius Dungeons</h2>
-                <h3>Full Stack Vue.js Web Game</h3>
-                <p>During my time as a student of Austin Coding Academy, I created a full stack web game using Vue.js and Google's Firebase Backend</p>
-                <a class="button" target="_blank" href="https://www.moebiusdungeons.com"> <Icon name="link" /> Play Moebius Dungeons</a>
+              <ResumeBlock
+                type="project"
+                title="Moebius Dungeons"
+                what="Full Stack Vue JS Web Game"
+                description="During my time as a student of Austin Coding Academy, I created a full stack web game using Vue.js and Google's Firebase Backend"
+                url="https://www.moebiusdungeons.com"
+                linkText="Play the Game!"
+                iconName="gamepad"
+              />
 
               </section>
             </section>
-
-
           </section>
-  </section>
 </template>
 
 <script>
@@ -111,11 +133,6 @@ import moment from 'moment';
 
 export default {
   name: 'ResumePage',
-  data() {
-    return {
-
-    }
-  },
   computed: {
     devLevel: function () {
       return moment(19930326, "YYYYMMDD").fromNow().slice(0, -9) - 1;
@@ -124,90 +141,8 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-.button{
-  display:block;
-  text-decoration:none;
-  background:rgb(0, 116, 124);
-  color:white;
-  font-family: var(--plain-font);
-  text-transform:uppercase;
-  padding:10px;
-  border-radius:10px;
-  margin:10px;
-}
-
-.button:hover {
-  background:rgb(0, 151, 161);
-}
-
-h1 {
-  font-size:10vw;
-  text-align:center;
-}
-
-h2 {
-  font-size:6vw;
-  text-align:left;
-}
-
-h3 {
-  font-size:4vw;
-  font-weight:600;
-  text-align:left;
-}
-
-h3.centerText {
-  text-align:center;
-}
-
-h4 {
-  font-size:3vw;
-  text-align:left;
-}
-
-ul li {
-  margin-bottom:5px;
-}
-
-.skills p {
-  background:rgb(243, 243, 243);
-  padding:10px 10px;
-  border-radius:999px;
-  margin:5px 5px;
-  text-indent: 0px;
-  color:black;
-  box-shadow: 2px 2px 1px rgb(206, 206, 206);
-  display:flex;
-  align-items:center;
-  justify-content: center;
-}
-
-.fa-icon {
-  margin-right:5px;
-}
-
-.skills p:not(:last-of-type){
-  margin-left:5px;
-}
-
-.block {
-  background:rgb(243, 243, 243);
-  padding:10px;
-  margin: 10px 0;
-  border-radius:10px;
-  box-shadow: 2px 2px 1px rgb(206, 206, 206);
-}
-
-.block p {
-  text-indent: 0;
-}
-
-.date {
-  font-style:italic;
-}
-
 .resume {
   display:grid;
   grid-template-columns: auto;
@@ -243,10 +178,6 @@ header {
   grid-area: experience;
 }
 
-.experience ul li{
-  text-align:left;
-};
-
 .education {
   grid-area: education;
 }
@@ -254,207 +185,49 @@ header {
 .personal {
   grid-area: personal;
 }
-
-
-/* Mobile Port */
-@media (min-width: 667px) {}
 
 /* Tablets and Up */
 @media (min-width: 768px) {
-.skills p {
-  background:rgb(243, 243, 243);
-  padding:10px 10px;
-  margin:5px 5px;
-  border-radius:999px;
-  text-indent: 0px;
-  color:black;
-  box-shadow: 2px 2px 1px rgb(206, 206, 206);
 
-}
-
-.block {
-  background:rgb(243, 243, 243);
-  padding:10px;
-  margin: 10px 0;
-  border-radius:10px;
-  box-shadow: 2px 2px 1px rgb(206, 206, 206);
-}
-
-.block p {
-  text-indent: 0;
-}
-
-.date {
-  font-style:italic;
-}
-
-.resume {
-  display:grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto;
-  column-gap: 10px;
-  width:75%;
-  margin:0 auto;
-  grid-template-areas: 
-    "header header header header header header"
-    "experience experience experience experience skills skills"
-    "education education education education personal personal"
-  ;
-}
-
-header, .skills, .experience, .education, .personal {
-  padding:10px;
-}
-
-header {
-  display:flex;
-  justify-content: center;
-  align-items:center;
-  flex-direction:column;
-  grid-area: header;
-  border-bottom:black 1px solid;
-}
-
-.skills {
-  grid-area: skills;
-}
-
-.experience {
-  grid-area: experience;
-}
-
-.education {
-  grid-area: education;
-}
-
-.personal {
-  grid-area: personal;
-}
 }
 
 @media (min-width: 1024px) {
+  .resume {
+    display:grid;
+    grid-template-columns: auto;
+    grid-template-rows: auto;
+    margin:0 auto;
+    grid-template-areas: 
+      "header header"
+      "experience skills"
+      "personal education"
+    ;
+  }
+
+  header h1, header h2, header h3 {
+    text-align:center;
+    margin:5px;
+  }
+
+  h1, h2, h3 {
+    text-align:left;
+  }
+
   h1 {
-  font-size:5vw;
-  margin-bottom:0px;
-  text-align:center;
-}
+    font-size:3vw;
+  }
 
-h2 {
-  font-size:3vw;
-  margin-bottom:10px;
-  text-align:left;
-}
+  h2 {
+    font-size:2vw;
+  }
 
-h3 {
-  font-size:1vw;
-  margin-bottom:10px;
-  font-weight:600;
-  text-align:left;
-}
+  h3 {
+    font-size:1.2vw;
+  }
 
-h3.centerText {
-  text-align:center;
-}
+  p {
 
-h4 {
-  font-size:3vw;
-  text-align:left;
-}
-
-.skills p {
-  background:rgb(243, 243, 243);
-  padding:10px 10px;
-  margin:5px 5px;
-  border-radius:999px;
-  font-size:1.4vw;
-}
-
-.block {
-  background:rgb(243, 243, 243);
-  padding:10px;
-  margin: 10px 0;
-  border-radius:10px;
-  box-shadow: 2px 2px 1px rgb(206, 206, 206);
-}
-
-.block p {
-  text-indent: 0;
-}
-
-.date {
-  font-style:italic;
-}
-
-.resume {
-  display:grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto;
-  column-gap: 10px;
-  width:80%;
-  margin:0 auto;
-  grid-template-areas: 
-    "header header header header header header"
-    "experience experience experience skills skills skills"
-    "personal personal personal education education education"
-  ;
-}
-
-header, .skills, .experience, .education, .personal {
-  padding:10px;
-}
-
-header {
-  display:flex;
-  justify-content: center;
-  align-items:center;
-  flex-direction:column;
-  grid-area: header;
-  border-bottom:black 1px solid;
-}
-
-.skills {
-  grid-area: skills;
-}
-
-.experience {
-  grid-area: experience;
-}
-
-.experience p{
-  text-align:left;
-}
-
-.experience h1 {
-  text-align: left;
-}
-
-.experience h3 {
-  font-size:2vw;
-}
-
-.education {
-  grid-area: education;
-}
-
-.education h1 {
-  text-align: left;
-}
-
-.education p {
-  text-align:left;
-}
-
-.personal {
-  grid-area: personal;
-}
-
-.personal h1 {
-  text-align: left;
-}
-
-.personal p {
-  text-align: left;
-}
+  }
 }
 
 </style>
