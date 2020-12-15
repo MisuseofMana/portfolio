6dd8fbb0dfe8a7ad77f2f7e0d7e9d8a3db1d94fd
Sean Yager <contact@seanyager.com>
2020-12-15 14:32:06 -0600

    new html layout to accomodate new css

--- pages/about.vue
+++ pages/about.vue
@@ -1,25 +1,27 @@
 <template>
     <div class="hero">
+        <section class="heroPadding">
             <section>
-                <h1>What's up!</h1>
                 <img class="imageFit" src="@/assets/imgs/propic.png" alt="">
+                <h1>What's up!</h1>
                 <p>I'm Sean Yager, a.k.a Shinbone, a.k.a Misuse of Mana, By day I code, create, and maintain a smattering of websites for my clients. By night I summon creatures, camaraderie, and conflicts to enrich my growing storytelling universe, the realm of Amara.</p>
                 <p>I grew up in the cornfields of Indiana swinging tree branch swords and imagining backyard adventures. I went to college at IUPUI and Herron School of Art for several years before deciding that student debt was going to be a big burden. I took a hard pivot to learn Web Development via Austin Coding Academy and now I work as a freelancer in art and web design to offset my bills. I'm looking for more permenant work though so if you need a consultant, or an artist, or a website remodel please reach out!</p>
                 <p>If you're interested in finding out more about my projects, I'm all over the place on social media just look me up!</p>
             </section>
 
-        <section class="flexRowAround">
-            <h1 class="handle">@MisuseofMana</h1>
-            <SocialMedia/>
+            <section class="flexRowAround">
+                <h1 class="handle">@MisuseofMana</h1>
+                <SocialMedia/>
+            </section>
         </section>
     </div>
 </template>
 
 <style scoped>
 img.imageFit{
-    width:70%;
+    width:50%;
     border-radius:999px;
-    margin:20px;
+    margin:0 10px 10px 10px;
     float:right;
 }
 
