c38db66e7ebd2c0978b8d3c4f7e9ddb9c62d59d7
Sean Yager <contact@seanyager.com>
2020-12-15 14:29:15 -0600

    +icons: changed heading tags

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,40 +1,40 @@
 <template>
-  <nav @click="toggleNav()">
-      <section class="navHeader">
-          <section class="flexRowBetween" key="header" >
-            <section class="title">
-              <h1>SEAN YAGER</h1>
-              <transition name="fade" mode="out-in">
-                <h2 class="thinFont" :key="randomTitle">{{randomTitle}}</h2>
-              </transition>
-            </section>
-            <Icon name="caret-square-down"/>
-          </section>
-      </section>
+	<nav>
+		<section class="flexRowBetween innerNavHeader" key="header" @click="toggleNav()">
+			<section class="title">
+			<h1>SEAN YAGER</h1>
+			<transition name="fade" mode="out-in">
+				<h2 class="thinFont" :key="randomTitle">{{randomTitle}}</h2>
+			</transition>
+			</section>
+			<Icon name="bars"/>
+		</section>
 
-      <transition name="growAppear" mode="out-in">
-        <section v-if="navOpen" key="navigation" class="menuContainer" >
-          <section class="flexRowBetween">
-              <section class="title">
-                <h1>NAVIGATION</h1>
-              </section>
-              <Icon name="window-close"/>
-            </section>
+		<transition name="growAppear" mode="out-in">
+			<section v-if="navOpen" key="navigation" class="menu" @click="toggleNav()">
+				<section class="innerMenuContainer">
+					<section class="flexRowBetween menuHeader">
+						<section class="title">
+							<h1>NAVIGATION</h1>
+						</section>
+						<Icon name="window-close"/>
+						</section>
 
-          <section class="flexColumn">
-            <NuxtLink to="/" exact><p>Home</p></NuxtLink>
-            <NuxtLink to="/about"><p>About</p></NuxtLink>
-            <NuxtLink to="projects/comics"><p>Comics</p></NuxtLink>
-            <NuxtLink to="projects/books"><p>Books</p></NuxtLink>
-            <NuxtLink to="projects/coding"><p>Coding</p></NuxtLink>
-            <NuxtLink to="projects/podcasts"><p>Podcasts</p></NuxtLink>
-            <NuxtLink to="projects/games"><p>Games</p></NuxtLink>
-            <NuxtLink to="projects/photography"><p>Photography</p></NuxtLink>
-            <NuxtLink to="/resume"><p>Resume</p></NuxtLink>
-          </section>
-        </section>
-      </transition>
-  </nav>
+					<section class="flexColumn menuLinks">
+						<NuxtLink class="flexRowCenter" to="/" exact><Icon name="home"/> Home</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="/about"><Icon name="address-card"/> About</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="projects/comics"><Icon name="book-open"/> Comics</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="projects/books"><Icon name="book"/> Books</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="projects/coding"><Icon name="code"/> Coding</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="projects/podcasts"><Icon name="microphone"/> Podcasts</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="projects/games"><Icon name="dice"/> Games</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="projects/photography"><Icon name="camera-retro"/> Photography</NuxtLink>
+						<NuxtLink class="flexRowCenter" to="/resume"><Icon name="envelope-open-text"/> Resume</NuxtLink>
+					</section>
+				</section>
+			</section>
+		</transition>
+	</nav>
 </template>
 
 <script>
@@ -45,7 +45,7 @@
           randomNumber: 0,
           titles: [
             ":Storyteller",
-            ":Comics Artist",
+            ":Artist",
             ":Designer",
             ":Web Dev",
             ":Maker",
@@ -81,63 +81,82 @@
 
 <style scoped>
 nav {
-  padding:0 20px;
   background-color:white;
   border-bottom:2px solid black;
   position: sticky;
   top:0;
   z-index:0;
+  width:100vw;
+  margin-bottom:10px;
 }
 
-.navHeader {
-  
+.innerNavHeader {
+    padding:10px;
 }
 
-.navHeaderOpen{
-  background-color:rgba(255,255,255);
-  position: sticky;
-  width: 100vw;
-  top:0;
-  left:0;
-  z-index:9999;
-  padding:0px 20px;
+.innerMenuContainer {
+    padding:0 10px;
+    height:100vh;
 }
 
 .title {
   display:flex;
   justify-content:flex-start;
   align-items:center;
+  height:50px;
+}
+
+.title h1 {
+  margin:0;
+  line-height:50px;
 }
 
 .title h2 {
-  margin-left:20px;
+  margin:0 0 0 10px;
+  line-height:50px;
 }
 
-.menuContainer {
-  position:absolute;
+.menu {
+  position:fixed;
   top:0;
   left:0;
   width:100vw;
-
   background:white;
+  z-index:9999;
+  padding:10px 0;
 }
 
-.menuContainer ul a.nuxt-link-exact-active li {
-  background:rgb(196, 218, 219);
+.menuHeader {
+  border-bottom:2px solid black;
+  margin-bottom:10px;
 }
 
-.menuContainer a:link {
-  color:rgb(0, 0, 0);
+.menuLinks a:link {
+  background:#eee;
+  width:75%;
+  text-align:center;
+  margin:5px 0;
+  padding:15px;
+  font-family:var(--h1-font);
+  font-weight:700;
+  border-radius:20px;
+  color:var(--primary-color);
 }
 
-.menuContainer a:visited {
-  color: var(--primary-color);
+.menuLinks a:visited {
+  color:var(--visited-links);
 }
 
-.menuContainer ul li:before {
-  content:": ";
+.menuLinks a.nuxt-link-exact-active {
+  background: var(--primary-color);
+  color:white;
 }
-.menuContainer ul li:after {
-  content:" :";
+
+.menuLinks a .fa-icon {
+	margin-right:5px;
+	width: auto;
+	height: 4vw; 
+	max-width: 100%;
+	max-height: 100%;
 }
 </style>
