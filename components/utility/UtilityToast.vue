114d83ef5acd68679a05bcd7c8c354d7fead9b83
Sean Yager <contact@seanyager.com>
2020-12-15 14:28:18 -0600

    started a new toast component

---
+++ components/utility/UtilityToast.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="toastBlock flexRowCenter">
+        <p>
+            {{ toastText }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['toastText'],
+    }
+</script>
+
+<style scoped>
+    .toastBlock {
+        position:fixed;
+        top:0;
+        bottom:0;
+        height:100vh;
+        width:100vw;
+        padding:10px;
+        margin:0 0 10px 0;
+        background:#eee;
+        border:1px solid black;
+    }
+</style>
