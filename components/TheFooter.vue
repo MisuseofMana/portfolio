043990648cd294ccb05e764caeec82c607fc1a00
Sean Yager <contact@seanyager.com>
2020-12-15 14:31:08 -0600

    changed html to make styling easier

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,19 +6,18 @@
             </section>
             
             <section class="links">
-                <h2>My Friends and Their Work</h2>
-                <ul class="flexRowWrap">
-                    <a target="_blank" href="https://www.instagram.com/threepanelorigin"><li>Dan Wolff</li></a>
-                    <a target="_blank" href="https://www.instagram.com/threepanelorigin"><li>Three Panel Origin</li></a>
-                    <a target="_blank" href="https://bakanobingo.com/"><li>Baka No Bingo</li></a>
-                    <a target="_blank" href="https://www.instagram.com/drawdillondraw"><li>DrawDillonDraw</li></a>
-                    <a target="_blank" href="https://www.instagram.com/treyzart"><li>TreyZArt</li></a>
-                    <a target="_blank" href="https://www.instagram.com/darcybabs"><li>DarcyBabs</li></a>
-                    <a target="_blank" href="https://www.twitter.com/moegeemoe"><li>MoeGeeMoe</li></a>
-                    <a target="_blank" href="https://www.instagram.com/joshhainesart"><li>JoshHainesArt</li></a>
-                    <a target="_blank" href="https://www.instagram.com/justinsmithart"><li>JustinSmithArt</li></a>
-
-                </ul>
+                <h2>My Friends &amp; Their Work</h2>
+                <section class="flexRowWrap">
+                    <a target="_blank" href="https://www.instagram.com/threepanelorigin">Dan Wolff</a>
+                    <a target="_blank" href="https://www.instagram.com/threepanelorigin">Three Panel Origin</a>
+                    <a target="_blank" href="https://bakanobingo.com/">Baka No Bingo</a>
+                    <a target="_blank" href="https://www.instagram.com/drawdillondraw">DrawDillonDraw</a>
+                    <a target="_blank" href="https://www.instagram.com/treyzart">TreyZArt</a>
+                    <a target="_blank" href="https://www.instagram.com/darcybabs">DarcyBabs</a>
+                    <a target="_blank" href="https://www.twitter.com/moegeemoe">MoeGeeMoe</a>
+                    <a target="_blank" href="https://www.instagram.com/joshhainesart">JoshHainesArt</a>
+                    <a target="_blank" href="https://www.instagram.com/justinsmithart">JustinSmithArt</a>
+                </section>
             </section>
 
             <section class="title">
@@ -34,15 +33,15 @@
 
 <style scoped>
     footer {
-        width:100%;
-        margin:0 auto;
+        width:100vw;
         background:white;
         text-align:center;
+        border-top: solid 2px black
     }
 
     .footerWrapper {
         width:90%;
-        padding:50px;
+        padding:10px;
         max-width:1400px;
         margin:0 auto;
         display:grid;
@@ -56,12 +55,8 @@
         "updated"
     }
 
-    .footerTop {
-        margin-bottom:30px
-    }
-
     .quote, .links, .title {
-        margin-bottom:50px;
+        margin-bottom:20px;
     }
     
     .quote {
@@ -72,23 +67,18 @@
         grid-area: links;
     }
 
-    .links ul {
-        list-style: none;
-    }
-
     .links a {
         text-decoration:none;
-    }
-
-    .links li {
-        border-radius:20px;
+        border-radius:10px;
+        padding:10px;
+        margin:10px;
         text-indent: 0px;
         color:black;
         background:rgb(243, 243, 243);
         box-shadow: 2px 2px 1px rgb(206, 206, 206);
     }
     
-    .links li:hover{
+    .links a:hover{
         cursor:pointer;
         background-color:var(--primary-color);
         color:white;
