49560ca203e5242f51489da7fb8ed63d5ddd5712
Sean Yager <contact@seanyager.com>
2020-12-15 14:32:19 -0600

    cleaning component

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
     <section class="hero">
+      <section class='heroPadding'>
+
       <h1>Hey There!</h1>
       <p>Nice to meet you! I'm Sean Yager, a web developer, a comics illustrator, a 3D artist, a poet, a storyteller, but mostly I'm a well versed, certified techology nerd.</p>
       <p>Stay a while and browse my myriad of projects, or tap the navbar for more!</p>
@@ -9,12 +11,7 @@
 
         <CardTemplateMobile/>
         <CardTemplateDesktop/>
-        
+      
+      </section>
     </section>
 </template>
-
-<style scoped>
-h1 {
-  text-align:left;
-}
-</style>
