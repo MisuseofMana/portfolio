e9fd8fc57ca7165f04df0f16e7fb395d8786003a
Sean Yager <contact@seanyager.com>
2020-12-16 16:00:21 -0600

    new distribution to launch

---
+++ dist/_nuxt/static/1608155979/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fresume","\u002Fprojects\u002Fcomics","\u002Fprojects\u002Fbooks","\u002Fprojects\u002Fcoding","\u002Fprojects\u002Fpodcasts","\u002Fprojects\u002Fphotography"]})
---
+++ dist/_nuxt/static/1608155979/projects/books/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/projects/books", {data:[{slug:"books"}],fetch:[],mutations:void 0});
