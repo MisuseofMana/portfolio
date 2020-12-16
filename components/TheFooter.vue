c3fb5ea416819bb7de37f19ab34106f7916cf1d2
Sean Yager <contact@seanyager.com>
2020-12-16 15:34:23 -0600

    adding classes to footer contetn

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -24,7 +24,7 @@
                 <h3><NuxtLink to="/">SEANYAGER.COM</NuxtLink></h3>
             </section>
 
-            <section class="updated">
+            <section class="updated flexColumn">
                 <p>©2020 Sean Yager · All rights reserved</p>
             </section>
         </section>
@@ -96,14 +96,61 @@
     }
 
 @media (min-width: 768px) {
-    
+    .footerWrapper {
+        width:90%;
+        padding:10px;
+        max-width:1400px;
+        margin:0 auto;
+        display:grid;
+        grid-column-gap: 10px;
+        grid-template-columns: 50% 50%;
+        grid-template-rows: auto;
+        grid-template-areas:
+        "quote links"
+        "title updated"
+    }
+
+    h1 {
+        font-size:10vw;
+    }
+
+    h2 {
+        font-size:5vw;
+    }
+
+    a {
+        font-size:3vw;
+    }
+
+    p {
+        font-size:3vw;
+        line-height:3.2vw;
+    }
 }
 
 @media (min-width: 1024px) {
     .footerWrapper {
+        padding:20px;
         grid-template-areas:
         "quote links"
         "title updated"
+    }
+
+    h1 {
+        font-size:5vw;
+    }
+
+    h2 {
+        font-size:2vw;
+    }
+
+    a {
+        font-size:2vw;
+    }
+
+    p {
+        font-size:2vw;
+        line-height:2.2vw;
     }
 
     .links li {
