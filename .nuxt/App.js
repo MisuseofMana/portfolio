e6800af15430202eb509ce8c4d231ee921336a88
Sean Yager <contact@seanyager.com>
2020-12-16 15:33:10 -0600

    nuxt build steps

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -110,6 +110,10 @@ export default {
     isFetching () {
       return this.nbFetching > 0
     },
+
+    isPreview () {
+      return Boolean(this.$options.previewData)
+    },
   },
 
   methods: {
