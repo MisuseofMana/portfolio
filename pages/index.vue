dc076d53027fbb96c4679a446b238a1f3acc96a6
Sean Yager <contact@seanyager.com>
2021-01-18 12:46:26 -0600

    testing linter

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <h1>Hey There!</h1>
       <p>Nice to meet you! I'm Sean Yager, a web developer, a comics illustrator, a 3D artist, a poet, a storyteller, but mostly I'm a well versed, certified technology nerd.</p>
       <p>Stay a while and browse my myriad of projects, or tap the navbar for more!</p>
-    
+
       <hr class="ifMobile">
       <br class="ifDesktop">
 
